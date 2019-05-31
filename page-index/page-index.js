97c0507eec3b2a2e08132623b057eeab40cc16fc
Blagoj Atanasovski <46002495+blagojts@users.noreply.github.com>
2019-05-31 15:08:29 +0200

    Timescale Output Plugin for telegraf (#150)
    Tutorial page for the "Collecting metrics with the Telegraf Output for PostgreSQL"

--- page-index/page-index.js
+++ page-index/page-index.js
@@ -366,6 +366,11 @@ const pageIndex = [
                 href: "prometheus-adapter",
                 children: []
             }, {
+                Title: "Collecting metrics with Telegraf",
+                type: PAGE,
+                href: "telegraf-output-plugin",
+                children: []
+            },{
                 Title: "Other sample datasets",
                 type: PAGE,
                 href: "other-sample-datasets",
