33d920281f1af544d0c4ec4a0ca07d9aec558565
Brian Rowe <brian@timescale.com>
2019-10-23 08:17:28 -0700

    Update the clustering tutorial (#236)
    This change updates the clustering tutorial to reflect the current
    APIs and address some common pitfalls that might be seen trying to
    update a clean postgres install.

--- page-index/page-index.js
+++ page-index/page-index.js
@@ -224,10 +224,10 @@ const pageIndex = [
                 type: PAGE,
                 href: "migrating-data"
             }, {
-            		Title: "Scaling Out",
-            		type: PAGE,
-            		href: "scaling-out"
-      	    }, {
+                    Title: "Scaling Out",
+                    type: PAGE,
+                    href: "scaling-out"
+            }, {
                 type: HIDDEN_REDIRECT,
                 href: "basic-operations",
                 to: "/using-timescaledb/hypertables"
@@ -444,6 +444,10 @@ const pageIndex = [
                 Title: "Telemetry",
                 type: PAGE,
                 href: "telemetry"
+            }, {
+                Title: "Limitations",
+                type: PAGE,
+                href: "limitations",
             }
         ]
     }, {
