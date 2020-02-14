d5be0be398167d08c755299aa4aa656c77a6e4a2
Solar Olugebefola <solar@iobeam.com>
2020-02-14 13:18:01 -0500

    [fix]Typo

--- page-index/page-index.js
+++ page-index/page-index.js
@@ -471,8 +471,9 @@ const pageIndex = [
         Title: "FAQ",
         type: PAGE,
         href: "faq"
-    }, {
-        Title: "Github",
+    },
+    {
+        Title: "GitHub",
         type: LINK,
         href: "https://github.com/timescale/timescaledb"
     }, {
