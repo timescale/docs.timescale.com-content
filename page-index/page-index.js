25590cd528878a34c7cf12091f484bc226afd575
CoolAssPuppy <prashant_sridharan@hotmail.com>
2020-03-02 15:47:29 -0800

    Add tutorial for Grafana. Starting point for more Grafana content.

--- page-index/page-index.js
+++ page-index/page-index.js
@@ -482,6 +482,11 @@ const pageIndex = [
                 href: "prometheus-adapter",
                 children: []
             }, {
+                Title: "Visualizing data with Grafana",
+                type: PAGE,
+                href: "tutorial-grafana",
+                children: []
+            }, {
                 Title: "Collecting metrics with Telegraf",
                 type: PAGE,
                 href: "telegraf-output-plugin",
