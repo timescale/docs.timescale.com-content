db4e8b29f5a895d6139ba695441ce74a792150a8
Solar Olugebefola <solar@iobeam.com>
2020-04-24 13:28:13 -0400

    [fix]Add redirect for new tutorial filename

--- page-index/page-index.js
+++ page-index/page-index.js
@@ -550,7 +550,11 @@ const pageIndex = [
                         href: "#in-depth-weather"
                     }
                 ]
-            }
+            }, {
+                type: HIDDEN_REDIRECT,
+                href: "tutorial-hello-nyc",
+                to: "/tutorials/tutorial-hello-timescale"
+            },
         ]
     }, {
         Title: "API reference",
